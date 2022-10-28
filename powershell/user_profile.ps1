1d0b3eba71d0905317db715ce70e193a79e4db9c
Milinda Barua <87975145+LegioN2004@users.noreply.github.com>
2022-10-28 19:41:19 +0530

    updated powershell config

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -1,44 +1,37 @@
-
-#Prompt
-Import-Module posh-git 
-Import-Module oh-my-posh 
-Set-PoshPrompt takuya
+# ohmyposh
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\powerlevel10k_modern.omp.json" | Invoke-Expression
 
 #PSReadLine
-Set-PSReadLineOption -EditMode Emacs
-Set-PSReadLineOption -BellStyle none
-Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
-Set-PSReadLineOption -PredictionSource History
+ Set-PSReadLineOption -EditMode Emacs
+ Set-PSReadLineOption -BellStyle none
+ Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+ Set-PSReadLineOption -PredictionSource History
 
 #Icons
 Import-Module -Name Terminal-Icons 
 
 #Fzf
-Import-Module PSFzf
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadLineChordReverseHistory 'Ctrl+r'
+ # Import-Module PSFzf
+ # Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadLineChordReverseHistory 'Ctrl+r'
 
 #Alias 
 Set-Alias grep findstr 
 Set-Alias ll ls
-Set-Alias g git
 Set-Alias touch New-Item
-Set-Alias svim 'vim -u C:\Users\sunny\Downloads\.SpaceVim\vimrc'
-Set-Alias nvimrc 'nvim C:\Users\sunny\AppData\Local\nvim\init.vim'
 Set-Alias v nvim
-#.. git aliases
-# Set-Alias gcl git clone --depth 1
-# Set-Alias gi git init
-# Set-Alias ga git add
-# Set-Alias gc git commit -m
-# Set-Alias gp git push #add branch name if needed
-# Set-Alias gs git status
+Set-Alias svim 'vim -u C:\Users\sunny\.SpaceVim\vimrc'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias vi 'C:\Program Files\Git\usr\bin\vi.exe'
+Set-Alias vim 'C:\Program Files\Git\usr\bin\vim.exe'
+Set-Alias svim 'vim -u C:\Users\sunny\.SpaceVim\vimrc'
+# Set-Alias gv gvim
+# Set-Alias nv neovide
 
-
-#Utilities
+# Utilities
 function which ($command){
     Get-Command -Name $command -ErrorAction SilentlyContinue |
      Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
 } 
 
-#starship
 # Invoke-Expression (&starship init powershell)
