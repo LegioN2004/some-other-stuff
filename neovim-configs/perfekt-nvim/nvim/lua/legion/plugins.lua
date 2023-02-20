9214aa80162ef10e5e25e9373bdb2cd0d3b67e4c
LegioN2004 <sunnybaruasins@gmail.com>
2023-02-20 23:04:35 +0530

    aorint

--- neovim-configs/perfekt-nvim/nvim/lua/legion/plugins.lua
+++ neovim-configs/perfekt-nvim/nvim/lua/legion/plugins.lua
@@ -97,7 +97,15 @@ return require 'packer'.startup(function()
 						require 'alpha'.setup(require 'alpha.themes.dashboard'.config)
 				end
 		}
-
+		use {
+				'phaazon/hop.nvim',
+				branch = 'v2', -- optional but strongly recommended
+				config = function()
+						-- you can configure Hop the way you like here; see :h hop-config
+						require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+				end
+		}
+		use 'justinmk/vim-sneak'
 
 		-- Automatically set up your configuration after cloning packer.nvim
 		-- Put this at the end after all plugins
